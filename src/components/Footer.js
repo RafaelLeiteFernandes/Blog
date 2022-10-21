e50c5b63188085c97563168d62fdac2e4f8b19e3
Rafaelzera7 <rafael.fernandesAIMEC@gmail.com>
2022-10-21 16:58:49 -0300

    save

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
         <h3>Escreva sobre o que você tem interesse!</h3>
-        <p>DEV's blog &copy; 2022</p>
+        <p>DEV's blog - Desenvolvido por Rafael Leite &copy; 2022</p>
     </footer>
   )
 }
